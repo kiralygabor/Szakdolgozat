0b7c4608ba0b6b5f9f1aa02d1bcb44681fb599e9
Gergely Nagy <152151245+ngergely0@users.noreply.github.com>
2025-09-11 12:46:44 +0200

    authorization

---
+++ storage/framework/views/ad7fb07aa4a02ebe9c517a71badd03ef.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osztálynapló</title>
+    <script src="<?php echo e(asset('js/jquery-3.7.1.min.js')); ?>"></script>
+    <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+ 
+    <link rel="shortcut icon" href="<?php echo e(asset('favicon.png')); ?>" type="image/x-icon">
+</head>
+ 
+<body>
+<header>
+
+
+</header>
+</header>
+
+
+    <main>
+        <?php echo $__env->yieldContent('content'); ?>
+    </main>
+ 
+    <footer>
+        <p>&copy; Király Gábor - Praszna Koppány - Nagy Gergely - 2025</p>
+    </footer>
+ 
+</body>
+ 
+</html><?php /**PATH C:\xampp\htdocs\minijobz\resources\views/layout.blade.php ENDPATH**/ ?>
